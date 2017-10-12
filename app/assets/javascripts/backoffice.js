ee606a96efa6c68ad901e5c21322aab97433a8b6
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2017-10-11 23:59:03 -0300

    Improve code

--- app/assets/javascripts/backoffice.js
+++ app/assets/javascripts/backoffice.js
@@ -1,11 +1,12 @@
 //= require materialize/extras/nouislider
 
 // $( document ).ready(function(){
-//   // Initialize collapse button
+//
 //   $(".button-collapse").sideNav();
 // })
 
 $( document ).on('turbolinks:load', function() {
   Waves.displayEffect();
- $(".button-collapse").sideNav();
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
 });
