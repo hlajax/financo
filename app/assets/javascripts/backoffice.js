bbf1655090aa56d58868d5e1203b8842f577e11f
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2017-10-11 23:57:30 -0300

    Fix button-collapse when the turbolinks is on

--- app/assets/javascripts/backoffice.js
+++ app/assets/javascripts/backoffice.js
@@ -1,6 +1,11 @@
 //= require materialize/extras/nouislider
 
-$( document ).ready(function(){
-  // Initialize collapse button
-  $(".button-collapse").sideNav();
-})
+// $( document ).ready(function(){
+//   // Initialize collapse button
+//   $(".button-collapse").sideNav();
+// })
+
+$( document ).on('turbolinks:load', function() {
+  Waves.displayEffect();
+ $(".button-collapse").sideNav();
+});
