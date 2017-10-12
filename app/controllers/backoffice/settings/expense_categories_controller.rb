a9b8471f4801d415cfa7b295262fadb1a3e6f087
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2017-10-12 05:16:56 -0300

    Create page for new expense categories

--- app/controllers/backoffice/settings/expense_categories_controller.rb
+++ app/controllers/backoffice/settings/expense_categories_controller.rb
@@ -1,4 +1,31 @@
 class Backoffice::Settings::ExpenseCategoriesController < BackofficeController
   def index
   end
+
+  def create
+    @expense_category = ExpenseCategory.new(params_expense_category)
+    if @expense_category.save
+      redirect_to backoffice_settings_path, notice: "The (#{@expense_category.name}) expense category was successfully created"
+    else
+      render :new
+    end
+  end
+
+  def new
+    @expense_category = ExpenseCategory.new
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
+
+  private
+
+  def params_expense_category
+    params.require(:expense_category).permit(:name)
+  end
 end
--- app/controllers/backoffice/settings/income_categories_controller.rb
+++ app/controllers/backoffice/settings/income_categories_controller.rb
@@ -1,4 +1,20 @@
 class Backoffice::Settings::IncomeCategoriesController < BackofficeController
   def index
   end
+
+  def create
+    #code
+  end
+
+  def new
+    @income_category = IncomeCategory.new
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
 end
