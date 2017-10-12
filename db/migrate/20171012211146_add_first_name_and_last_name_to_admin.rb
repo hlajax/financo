15ccb589095c92d5c5a1e2d1b2435fb35bd78359
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2017-10-12 18:28:00 -0300

    Add first_name and last_name to devise user and devise admin

---
+++ db/migrate/20171012211146_add_first_name_and_last_name_to_admin.rb
@@ -0,0 +1,6 @@
+class AddFirstNameAndLastNameToAdmin < ActiveRecord::Migration[5.1]
+  def change
+    add_column :admins, :first_name, :string
+    add_column :admins, :last_name, :string
+  end
+end
