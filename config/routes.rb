892e0775618cfee210a8a85d6188d901dd188410
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2017-10-14 13:03:31 +0000

    Change root router to backoffice

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
 
-  root 'site/home#index'
+  # root 'site/home#index'
+  root 'backoffice/dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
