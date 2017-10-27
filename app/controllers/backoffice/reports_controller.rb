cb8ec1cc6582b2d9c039314d75f1102759db5091
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2017-10-26 23:12:11 -0300

    Create reports controller

---
+++ app/controllers/backoffice/reports_controller.rb
@@ -0,0 +1,4 @@
+class Backoffice::ReportsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :backoffice do
+    get 'reports/index'
+  end
+
+  namespace :backoffice do
     get 'transactions/index'
   end
 
